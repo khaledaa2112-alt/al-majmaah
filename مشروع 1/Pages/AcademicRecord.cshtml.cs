364cb49a590a190db618dac68b2441f8a19ab119
khaledaa2112-alt <khaledaa2112@gmail.com>
2026-03-02 03:14:27 +0300

    Delete مشروع 1 directory

--- مشروع 1/Pages/AcademicRecord.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace مشروع_1.Pages
-{
-    public class AcademicRecordModel : PageModel
-    {
-        public void OnGet() { }
-    }
-}
--- مشروع 1/Pages/StudentDashboard.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace مشروع_1.Pages
-{
-    public class StudentDashboardModel : PageModel
-    {
-        public void OnGet() { }
-    }
-}
